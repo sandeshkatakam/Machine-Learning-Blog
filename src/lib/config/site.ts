2a92fd8e99cb69113fb3902b9ed0dca967cd05be
Sandesh Katakam <88609382+sandeshkatakam@users.noreply.github.com>
2023-05-21 12:57:07 +0530

    Update site.ts

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -3,15 +3,15 @@ import type { SiteConfig } from '$lib/types/site'
 export const site: SiteConfig = {
   protocol: import.meta.env.URARA_SITE_PROTOCOL ?? import.meta.env.DEV ? 'http://' : 'https://',
   domain: import.meta.env.URARA_SITE_DOMAIN ?? 'urara-demo.netlify.app',
-  title: 'Urara',
-  subtitle: 'Sweet & Powerful SvelteKit Blog Template',
+  title: 'Sandesh Katakam',
+  subtitle: 'My Machine Learning Blog',
   lang: 'en-US',
   description: 'Powered by SvelteKit/Urara',
   author: {
-    avatar: '/assets/maskable@512.png',
-    name: 'John Doe',
+    avatar: '/assets/profile_pic.png',
+    name: 'Sandesh Katakam',
     status: '🌸',
-    bio: 'lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    bio: 'Loves to write about latest research in Machine Learning and Deep Learning'
   },
   themeColor: '#3D4451'
 }
